05b033c2d5bd19a613f92d21ce6fca4945b6abfd
Chris <christophersstjohn@gmail.com>
2021-06-18 15:58:15 -0700

    Basic response

---
+++ test-dapp/src/types/index.ts
@@ -0,0 +1,12 @@
+//types/index.ts
+
+export interface Gotchi {
+	collateral: string;
+	id: string;
+	name: string;
+	withSetsNumericTraits: Array<Number>;
+ }
+ 
+ export interface QueryResponse {
+	aavegotchis: Array<Gotchi>
+ }
