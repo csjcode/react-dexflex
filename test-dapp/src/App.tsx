4ea59cc7348142a42b2961c42028b76c400b222f
Chris <christophersstjohn@gmail.com>
2021-06-18 16:16:06 -0700

    Display in list component

--- test-dapp/src/App.tsx
+++ test-dapp/src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
 import { Gotchi, QueryResponse } from './types';
+import { GotchiListing } from './components/GotchiListing';
 import { request } from "graphql-request";
 import './App.css';
 
 const uri = 'https://api.thegraph.com/subgraphs/name/aavegotchi/aavegotchi-core-matic';
-
 
 function App() {
 
@@ -33,7 +33,19 @@ function App() {
           <div className="container">
             <div className="selected-container">
             </div>
-            <div className="gotchi-list">
+            <div className="gotchi-list">              
+              {
+                gotchis.map((gotchi, i) => (
+                  <GotchiListing
+                    key={gotchi.id}
+                    id={gotchi.id}
+                    name={gotchi.name}
+                    collateralColor="black"
+                    selectGotchi={() => null}
+                    selected={false}
+                  />
+                ))
+              }
             </div>
           </div>
         </div>
